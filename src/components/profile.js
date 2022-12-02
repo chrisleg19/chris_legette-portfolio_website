bdc48f9b7c2530d441e5b4d9ea94de986296f917
chrisleg19 <110962234+chrisleg19@users.noreply.github.com>
2022-12-02 10:05:45 -0500

    update

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,11 +1,32 @@
+import Pic from "../resources/profilePicture.png"
+// import Image from 'react-bootstrap/../../src/resources/profilePicture.png'
+
+import {Image} from "react-bootstrap"
+
+
 
 const Profile = () =>{
     return(
-        <div>
+        <body>
+
+        <head>
+            <link rel="stylesheet" href=""/>
+        </head>
+
             <h1>
                 Profile Component
             </h1>
-        </div>
+
+            <img src={Pic} alt="" className='img-fluid rounded thumbnail' style={{width: "300px"}}/>
+            
+            
+            <img
+      src={Pic}
+        className='img-fluid rounded'
+      alt='...'
+    />
+
+        </body>
     )
 
 }
